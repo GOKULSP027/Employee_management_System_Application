31bf09bf083c269c05e5f2ca3a6be1f578138894
GOKUL P <126493183+GOKULSP027@users.noreply.github.com>
2025-02-28 02:13:17 +0530

    Add files via upload

---
+++ database data/inser_data.sql
@@ -0,0 +1,29 @@
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('John', 'Doe', 'john.doe@gslogics.com', 60000, 'IT', 'Software Engineer', '2024-02-27');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Sarah', 'Smith', 'sarah.smith@gslogics.com', 75000, 'Human Resources', 'HR Manager', '2023-08-15');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Michael', 'Brown', 'michael.brown@gslogics.com', 68000, 'Analytics', 'Data Analyst', '2022-05-10');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Jessica', 'Taylor', 'jessica.taylor@gslogics.com', 90000, 'Product Management', 'Product Manager', '2021-11-23');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Daniel', 'Wilson', 'daniel.wilson@gslogics.com', 85000, 'IT', 'DevOps Engineer', '2020-07-01');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Emily', 'Anderson', 'emily.anderson@gslogics.com', 55000, 'Marketing', 'Marketing Executive', '2023-04-12');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('David', 'Martinez', 'david.martinez@gslogics.com', 95000, 'Finance', 'Finance Manager', '2019-03-18');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Sophia', 'Garcia', 'sophia.garcia@gslogics.com', 45000, 'Customer Support', 'Customer Support Executive', '2023-09-05');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('James', 'Lopez', 'james.lopez@gslogics.com', 78000, 'Cybersecurity', 'Cybersecurity Analyst', '2022-01-30');
+
+INSERT INTO employees_table (fname, lname, email, salary, department, designation, joiningDate) 
+VALUES ('Olivia', 'Harris', 'olivia.harris@gslogics.com', 70000, 'Design', 'UX/UI Designer', '2021-06-22');
